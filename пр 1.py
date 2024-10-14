4c79f581317167b9d3519101c4a4d0372c2a8fd6
Sofiia1508 <sofiamartyniuk2008@gmail.com>
2024-10-14 12:48:26 +0300

    Add files via upload

---
+++ пр 1.py
@@ -0,0 +1,14 @@
+а = "Hello World" 
+name = "Sofiia" 
+prizv = "Martyniuk" 
+age = 16 
+ 
+print('a') 
+print(name) 
+print(prizv) 
+print(age) 
+ 
+print(type('a')) 
+print(type(name)) 
+print(type(prizv)) 
+print(type(age))
